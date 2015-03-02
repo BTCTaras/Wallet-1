4396c0a9706c5cdac8b753671fa68a47c47ff77e
Connor Delaney <connordelaneyy@gmail.com>
2015-03-02 20:00:03 +1100

    Update wallet.lua

--- src/wallet.lua
+++ src/wallet.lua
@@ -10,9 +10,14 @@ if not term.isColour() then
   error("Wallet requires a display that supports colour, try using an advanced computer.", 0)
 end
 
-local pkey = nil
+local uData = {
+  pkey = nil
+  balance = 0
+}
+
 local build = .5
 local autoUpdate = true
+local url = "http://65.26.252.225/quest/dia/krist/"
 local cBuild = http.get("https://raw.githubusercontent.com/connordelaneyy/Wallet/master/BUILD").readAll()
 
 local header = function()
@@ -34,10 +39,19 @@ end
 local login = function()
   header()
   term.setCursorPos(2, 4)
-  print("Enter your Private Key to use Krist, If this is\n your first time using Krist, a wallet will be\n generated with the Private Key you enter.")
+  print("Enter your Private Key to use Krist. If this is\n your first time using Krist, a wallet will be\n generated with the Private Key you enter.")
   term.setCursorPos(2, 8)
   write("Private Key: ")
-  pkey = read("*")
+  uData.pkey = read("*")
+end
+
+local menu = function()
+  local uData.balance = http.get(url..?balance..uData.pkey)
+  
+  header()
+  term.setCursorPos(2, 4)
+  print("Balance: "..uData.balance)
 end
 
 login()
+balance()
