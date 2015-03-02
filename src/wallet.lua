40a7be3bbd2753d67132543cf4138161aec84b00
Connor Delaney <connordelaneyy@gmail.com>
2015-03-02 19:00:38 +1100

    Update wallet.lua

--- src/wallet.lua
+++ src/wallet.lua
@@ -13,16 +13,18 @@ end
 local build = .5
 local cBuild = http.get("https://raw.githubusercontent.com/connordelaneyy/Wallet/master/BUILD").readAll()
 
-if build < tonumber(cBuild) then
-  local file = fs.open(shell.getRunningProgram(), "w")
-  file.write(http.get("https://raw.githubusercontent.com/connordelaneyy/Wallet/master/src/wallet.lua").readAll())
-  file.close()
-end
-
 local header = function()
   term.setBackgroundColour(colours.grey)
   term.setTextColour(colours.lightGrey)
   term.clear()
   term.setCursorPos(2, 2)
   print("Wallet - Created by CDEL")
+end
+
+header()
+
+if build < tonumber(cBuild) then
+  local file = fs.open(shell.getRunningProgram(), "w")
+  file.write(http.get("https://raw.githubusercontent.com/connordelaneyy/Wallet/master/src/wallet.lua").readAll())
+  file.close()
 end
